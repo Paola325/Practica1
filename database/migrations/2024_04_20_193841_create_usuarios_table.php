117dd5fa1d17f335e3fb822b50b39cbf620ee27f
Paola325 <paolitaperezlopez0@gmail.com>
2024-04-20 14:40:32 -0600

    mi rama

---
+++ database/migrations/2024_04_20_193841_create_usuarios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use PhpParser\Node\Expr\Cast\String_;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string("email");
+            $table->string("password");
+            $table->enum('role', ['Encargado', 'Cliente', 'Contador', 'Supervisor', 'Vendedor']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
