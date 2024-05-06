026409034bffb49ef1baca46874636d73e825204
EduardoVazG <eduardo_vazg@outlook.com>
2024-05-06 13:52:58 -0600

    .

--- resources/views/supervisor.blade.php
+++ resources/views/supervisor.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página Principal del Supervisor</title>
+    <title>Bienvenido Supervisor</title>
     <style>
         .container {
             display: flex;
